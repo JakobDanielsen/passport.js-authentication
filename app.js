01edca2d14b72544dbf0e680949ed3c6095d3fb2
© Jakob 3301™ <91836235+JakobDanielsen@users.noreply.github.com>
2022-04-26 21:28:07 +0200

    day 2

--- app.js
+++ app.js
@@ -1,35 +1,60 @@
 //jshint esversion:6
+
+//NPM PACKETS VI ØNSKER Å BRUKE SOM STORES I KONSTANTE VARIABLER
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
 const res = require("express/lib/response")
+const session = require('express-session')
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 
-mongoose.connect("mongodb://localhost:27017/loginsDB", {useNewUrlParser: true})
-const infoSchema = new mongoose.Schema({
+
+const app = express() // SETTER OPP EXPRESS SLIK AT VI KAN BRUKE METODER SOM USE,SET,POST,GET OG LISTEN
+
+app.use(express.static("public"))
+app.set("view engine","ejs")
+app.use(bodyParser.urlencoded({
+    extended:true // OFTE NÅR NOE ER SKREVET SLIKT ER DET FOR Å FORMATERE UTKOMMET PÅ EN ANNET MÅTE, OFTE GRUNNET DEPRIKASJON
+}))
+
+
+app.use(session({
+    secret:"hemmelighet",
+    resave: false,
+    saveUninitialized:false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose.connect("mongodb://localhost:27017/loginsDB", {useNewUrlParser: true}) // HER KOBLER VI OSS TIL DATABASEN VI ØNSKER Å BRUKE
+
+// MAL PÅ HVORDAN DATA SKAL SAMLES INN I MONGODB
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
 
-const User = mongoose.model("User",infoSchema)
+// BRUKER PASSPORT.JS SOM GJØR MYE ARBEID FOR OSS
+userSchema.plugin(passportLocalMongoose)
+
+const User = mongoose.model("User",userSchema)
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const user = new User ({
     email: "john.appleseed@gmail.com",
     password: "Password123"
 })
 
-// user.save()
 
 
-const app = express()
-
-app.use(express.static("public"))
-app.set("view engine","ejs")
-app.use(bodyParser.urlencoded({
-    extended:true
-}))
-
-// APP.GETS (
+// APP.GETS - SENDER EN BRUKER FILER BASERT PÅ HVA SOM STÅR I SØKEFELTET
 app.get("/",(req,res)=>{
     res.render("home")
 })
@@ -38,42 +63,60 @@ app.get("/login",(req,res)=>{
     res.render("login")
 })
 
+app.get("/secrets",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("secrets")
+    } else {
+        res.redirect("/login")
+    }
+    
+})
+
 app.get("/register",(req,res)=>{
     res.render("register")
 })
-// APP.GETS )
 
+app.get("/logout",(req,res)=>{
+    res.logout()
+    res.redirect("/")
+})
+
+// APP POSTS - BEHANDLER SUBMITS FRA FORMS OG KAN HENTE DATA UT FRA DEM
 app.post("/register", (req,res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
-    newUser.save((err)=>{
+
+    User.register({username: req.body.username},req.body.password,(err,user)=>{
         if(err){
             console.log(err);
+            res.redirect("/register")
         } else {
-            res.render("secrets")
+            passport.authenticate("local")(req,res, ()=>{
+                res.redirect("/secrets")
+            })
         }
     })
+
 })
 
 app.post("/login",(req,res)=>{
-    const username = req.body.username
-    const password = req.body.password
 
-    User.findOne({email:username},(err,foundUser)=>{
-        if(err){
+    const user = new User({
+        username:req.body.username,
+        password:req.body.password
+    })
+
+    req.login(user,(err)=>{
+        if (err) {
             console.log(err);
         } else {
-            if(foundUser){
-                if(foundUser.password === password) {
-                    res.render("secrets")
-                }
-            }
+            passport.authenticate("local")(req,res,()=>{
+                res.redirect("/secrets")
+            })
         }
     })
 })
 
+
+// APP LISTEN
 app.listen("3000",()=>{
     console.log("server started on 3000");
 })
